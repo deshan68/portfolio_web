6f76e918af49c1ecc8adcac9f21a8e9178534123
deshan68 <arundeshan@gmail.com>
2022-01-03 23:41:43 +0530

    my skills section completed

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ let index = 0;
 let currentText = '';
 let letter = '';
 
+var htmlVal=0;
+var sqlVal = 0;
+var cssVal =0 ; 
+var jsVal  = 0 ;
+var isFirst = true;
+
 (function type(){
     
     if(count === text.length){
@@ -43,6 +49,13 @@ function scrollAppear(){
     var item2 = document.querySelector('.item2');
     var item3 = document.querySelector('.item3');
 
+    //my skillsa title animation
+    var skTitle = document.querySelector('.sk-title');
+    var skSubTitle = document.querySelector('.sk-sub-title');
+
+    //my skills content animation
+    var skText = document.querySelector('.skills-text');
+
 
     var dpImg = document.querySelector('.mydp');
     var txtContent = document.querySelector('.text-content');
@@ -52,6 +65,9 @@ function scrollAppear(){
 
     var myService = document.querySelector('.my-service-main');
     var myServicetPosition = myService.getBoundingClientRect().top;
+
+    var mySkills = document.querySelector('.my-skills-main');
+    var mySkillsPosition = mySkills.getBoundingClientRect().top;
 
     var screenPosition = window.innerHeight / 1.2 ;
     
@@ -80,7 +96,69 @@ function scrollAppear(){
         item2.classList.add('item2-appear');
         item3.classList.add('item3-appear');
 
+    }
 
+    if(mySkillsPosition < screenPosition){
+        skTitle.classList.add('sk-title-appear');
+        skSubTitle.classList.add('sk-sub-title-appear');
+        skText.classList.add('skills-text-appear');
+
+        var htmlAnimi = document.querySelector('.Html');
+        var cssAnimi = document.querySelector('.css');
+        var jsAnimi = document.querySelector('.js');
+        var sqlAnimi = document.querySelector('.sql');
+
+        htmlAnimi.style.animation = 'value_bar_html 2s ease';
+        cssAnimi.style.animation = 'value_bar_css 2s ease';
+        jsAnimi.style.animation = 'value_bar_js 2s ease';
+        sqlAnimi.style.animation = 'value_bar_sql 2s ease';
+
+
+        // ---------------skils bars
+        if(isFirst == true){
+            (function htmlCount(){
+            
+                document.querySelector('.value-html').textContent = htmlVal + '%';
+                htmlVal++;
+                if(htmlVal <= 60){
+                    setTimeout(htmlCount, 16.66666666666667 *2);      
+                }
+    
+            }());
+    
+            (function cssCount(){
+                
+                document.querySelector('.value-css').textContent = cssVal + '%';
+                cssVal++;
+                if(cssVal <= 85){
+                    setTimeout(cssCount, 11.76470588235294 *2);      
+                }
+    
+            }());
+    
+            (function jsCount(){
+                
+                document.querySelector('.value-js').textContent = jsVal + '%';
+                jsVal++;
+                if(jsVal <= 45){
+                    setTimeout(jsCount, 22.22222222222222 *2);      
+                }
+    
+            }());
+            (function sqlCount(){
+                
+                document.querySelector('.value-sql').textContent = sqlVal + '%';
+                sqlVal++;
+                if(sqlVal <= 90){
+                    setTimeout(sqlCount,11.11111111111111 * 2);      
+                }
+    
+            }());
+
+            isFirst = false;
+        }
+
+        
     }
 }
 window.addEventListener('scroll',scrollAppear);
